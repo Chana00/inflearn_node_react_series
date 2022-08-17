eceb7ea9cb9babc07533f72c402e99d9d16cbb9d
yewon.ko <yewon.ko@hancom.com>
2022-08-17 17:36:03 +0900

    비밀 정보 보호

--- basic/index.js
+++ basic/index.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+//application/x-www/form-urlencoded 데이터를 분석해서 가져올 수 있게 도와준다
+app.use(bodyParser.urlencoded({extended : true}));
+
+// application/json 데이터를 분석해서 가져올 수 있게 도와준다
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yewonko:asdf1234@basic.mo4nkh6.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI, {dbName:'node-react-basic'})
 .then(() =>{ console.log("MongoDB connected...")} )
 .catch(err => console.log(err))
 
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello world! 안녕하세요 get 접속입니다~')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+user.save((err, uesrInfo) => {
+  if(err) return res.json({ success: false, err });
+
+  console.log("회원가입 POST 완료");
+  return res.status(200).json({
+    success: true
+  });
+});
 })
 
 app.listen(port, () => {
