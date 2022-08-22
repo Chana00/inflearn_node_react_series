c103490138528c887c2335bc05a03c088d3a85cf
고예원 <yewon.ko@hancom.com>
2022-08-22 16:40:28 +0900

    react/redux 회원가입&로그아웃 연동

--- basic/client/src/_actions/user_action.js
+++ basic/client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
     const req = axios.post('/api/users/login', dataToSubmit)
@@ -7,6 +7,16 @@ export function loginUser(dataToSubmit) {
 
     return {
         type: LOGIN_USER,
+        payload: req
+    }
+}
+
+export function registerUser(dataToSubmit) {
+    const req = axios.post('/api/register', dataToSubmit)
+        .then(res => res.data)
+
+    return {
+        type: REGISTER_USER,
         payload: req
     }
 }
--- basic/client/src/components/views/LandingPage/Landingpage.js
+++ basic/client/src/components/views/LandingPage/Landingpage.js
@@ -1,18 +1,39 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Landingpage() {
+    const navigate = useNavigate();
+
     useEffect(() => {
         axios.get('/api/hello')
             .then(res => { console.log(res) })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+            .then(res => {
+                console.log("로그아웃 시도");
+                console.log(res.data);
+                if (res.data.success) {
+                    navigate('/login');
+                } else {
+                    alert("로그아웃 실패");
+                }
+            })
+    }
 
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh'
         }}
-        > Landingpage</div >
+        > 시작페이지
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
+        </div >
     )
 }
 
