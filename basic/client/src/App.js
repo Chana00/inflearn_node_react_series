7662e918df5ecb6b3af083046961881be43e57d8
고예원 <yewon.ko@hancom.com>
2022-08-22 17:34:58 +0900

    user auth check

--- basic/client/src/App.js
+++ basic/client/src/App.js
@@ -11,8 +11,12 @@ import {
 import LandingPage from './components/views/LandingPage/Landingpage'
 import LoginPage from './components/views/loginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc/auth.js'
 
 function App() {
+  const AuthLandingPage = Auth(LandingPage, null);
+  const AuthLoginPage = Auth(LoginPage, false);
+  const AuthRegisterPage = Auth(RegisterPage, false);
   return (
     <Router>
       <div>
@@ -25,9 +29,9 @@ function App() {
           <Route path="/dashboard"> <Dashboard /> </Route>
         </Switch> */}
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/" element={<AuthLandingPage />} />
+          <Route exact path="/login" element={<AuthLoginPage />} />
+          <Route exact path="/register" element={<AuthRegisterPage />} />
         </Routes>
       </div>
     </Router>
