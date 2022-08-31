4b7a8a3278983ff0ed088640c65fe414709cb13a
고예원 <yewon.ko@hancom.com>
2022-08-31 13:35:04 +0900

    manage: Component생성

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -1,13 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id': 1,
+  'name': '홍길동',
+  'image': 'https://placeimg.com/64/64/1',
+  'birthday': '981222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'name': '고예원',
+  'image': 'https://placeimg.com/64/64/2',
+  'birthday': '991120',
+  'gender': '여자',
+  'job': '개발자'
+},
+{
+  'id': 3,
+  'name': '이순신',
+  'image': 'https://placeimg.com/64/64/3',
+  'birthday': '940312',
+  'gender': '남자',
+  'job': '대학생'
+},
+
+]
 
 function App() {
   return (
-    <div className="App gray-background">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Let's develop management system</h2>
-      </header>
+    <div>
+      {customers.map(c => {
+        return (<Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          job={c.job}
+        />);
+      })
+      }
     </div>
   );
 }
