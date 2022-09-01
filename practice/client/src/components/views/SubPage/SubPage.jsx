cb23a6099306c384453d06011815af66dba1cde4
고예원 <yewon.ko@hancom.com>
2022-09-01 17:26:52 +0900

    prac: monaco editor 사용자 지정 테마 설정

--- practice/client/src/components/views/SubPage/SubPage.jsx
+++ practice/client/src/components/views/SubPage/SubPage.jsx
@@ -1,22 +1,41 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Editor from '@monaco-editor/react'
+import Editor, { useMonaco } from '@monaco-editor/react'
 import "./subpage.scss"
 
 
 function SubPage() {
+  const monaco = useMonaco();
   const editorRef = useRef(null);
   const [Value, setValue] = useState('')
-  const codeText = `const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );`;
+  const codeText = `const test = 'it is test context!';`;
+
+  useEffect(() => {
+    
+    
+  }, []);
+  
+
   
   function handleEditorDidMount(editor, monaco) {
+    monaco.editor.defineTheme("webhwpTheme",{
+      base: 'vs',
+	inherit: true,
+	rules: [{ background: 'EDF9FA' }],
+	colors: {
+		'editor.foreground': '#000000',
+		'editor.background': '#EDF9FA',
+		'editorCursor.foreground': '#8B0000',
+		'editor.lineHighlightBackground': '#0000FF20',
+		'editorLineNumber.foreground': '#008800',
+		'editor.selectionBackground': '#88000030',
+		'editor.inactiveSelectionBackground': '#88000015'
+    }}
+    );
+    monaco.editor.setTheme('webhwpTheme');
+
     editorRef.current = editor; 
   }
 
@@ -37,6 +56,7 @@ function SubPage() {
         <Editor height='500px' width='600px'
           defaultLanguage="javascript"
           defaultValue={codeText}
+          theme="webhwpTheme"
           onMount={handleEditorDidMount}
       />
       <Box>{Value}</Box>
