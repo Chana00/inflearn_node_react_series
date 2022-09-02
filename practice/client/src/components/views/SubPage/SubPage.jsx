cf814433ea121fd594cf8f57af71631885b5baf0
고예원 <yewon.ko@hancom.com>
2022-09-02 11:01:41 +0900

    prac: monacoEditor theme 적용 및 초기화버튼

--- practice/client/src/components/views/SubPage/SubPage.jsx
+++ practice/client/src/components/views/SubPage/SubPage.jsx
@@ -3,40 +3,31 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Editor, { useMonaco } from '@monaco-editor/react'
+import MonokaiTheme from 'monaco-themes/themes/Monokai Bright.json'
 import "./subpage.scss"
 
 
+
 function SubPage() {
-  const monaco = useMonaco();
   const editorRef = useRef(null);
   const [Value, setValue] = useState('')
   const codeText = `const test = 'it is test context!';`;
 
   useEffect(() => {
     
-    
+
   }, []);
   
 
   
   function handleEditorDidMount(editor, monaco) {
-    monaco.editor.defineTheme("webhwpTheme",{
-      base: 'vs',
-	inherit: true,
-	rules: [{ background: 'EDF9FA' }],
-	colors: {
-		'editor.foreground': '#000000',
-		'editor.background': '#EDF9FA',
-		'editorCursor.foreground': '#8B0000',
-		'editor.lineHighlightBackground': '#0000FF20',
-		'editorLineNumber.foreground': '#008800',
-		'editor.selectionBackground': '#88000030',
-		'editor.inactiveSelectionBackground': '#88000015'
-    }}
-    );
-    monaco.editor.setTheme('webhwpTheme');
-
-    editorRef.current = editor; 
+    editorRef.current = editor;
+    let customMonokai = JSON.parse(JSON.stringify(MonokaiTheme))
+    console.log(MonokaiTheme)
+  
+    
+    monaco.editor.defineTheme("monokai", MonokaiTheme)
+    monaco.editor.setTheme('monokai')
   }
 
   function getValue(){
@@ -44,20 +35,41 @@ function SubPage() {
     setValue(content)
   }
 
+  function textInit() {
+    const content = editorRef.current.setValue('');
+    setValue(content);
+    
+  }
+
   return (
     <Box id='SubPage'>
       <p>SubPage Box</p>
       <Stack className="btn-stack" direction="row" spacing={12}>
         <Button className='ex-button' variant="contained" onClick={getValue}>실행</Button>
-        <Button className='ex-button' variant="contained">초기화</Button>
+        <Button className='ex-button' variant="contained" onClick={textInit}>초기화</Button>
       </Stack>
 
       <Stack direction="row" spacing={12}>
-        <Editor height='500px' width='600px'
+        <Editor height='600px' width='600px'
           defaultLanguage="javascript"
           defaultValue={codeText}
-          theme="webhwpTheme"
+          theme="monokai"
           onMount={handleEditorDidMount}
+          options={
+            {
+              fontSize: 16,
+              minimap: {enabled: false},
+              scrollbar: {
+                vertical: 'auto',
+                horizontal: 'auto'
+              },
+              contextmenu : true,
+              padding : {
+                top: 15,
+                bottom : 10
+              }
+            }
+          }
       />
       <Box>{Value}</Box>
       </Stack>
