9a30c8ae917445d9bc5f5b993ef7d9d57b4bff9d
고예원 <yewon.ko@hancom.com>
2022-09-07 17:28:52 +0900

    prac: 초기화 버튼 클릭시 example.js Editor 반영 문제 해결

--- practice/client/src/components/views/SubPage/SubPage.jsx
+++ practice/client/src/components/views/SubPage/SubPage.jsx
@@ -11,9 +11,6 @@ import "./subpage.scss"
 
 // 지금 문제점 두개
 // 1. example.js를 불러올 때 비동기 때문에 종종 example.js를 불러오는 것보다 페이지가 먼저 렌더링 되는 경우 발생
-// 2. 초기화 버튼을 눌렀을 때 monaco editor의 내용을 example.js의 코드로 초기화해야 하는데 monaco 초기화 코드를 지금 모름 ㅠ
-  //1. 내용만 초기화한다( 방법을 못찾았음 없음;; )
-  //2. 초기 세팅 모델 자체를 저장해두었다가 초기화 클릭시 현재 모델을 파괴하고 저장해두었던 세팅 모델을 불러온다
 function SubPage() {
   const editorRef = useRef(null);
   const [Value, setValue] = useState('')
@@ -64,10 +61,8 @@ function SubPage() {
   }
 
   function textInit() {
-    editorRef.current.defaultValue = ExampleScript;
+    editorRef.current.setValue(ExampleScript);
     setValue(InitScript);
-    
-    
   }
 
   return (
@@ -82,7 +77,6 @@ function SubPage() {
         <Editor height='860px' width='480px'
           defaultLanguage="javascript"
           defaultValue={ExampleScript}
-          onChange={setValue}
           theme="monokai"
           onMount={handleEditorDidMount}
           options={
